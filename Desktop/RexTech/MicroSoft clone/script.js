8a085133954ba8a0ef51d1cd697dc5babec382e2
Lubwama-Emmanuel <92721634+Lubwama-Emmanuel@users.noreply.github.com>
2022-02-07 15:51:17 +0300

    first commit

---
+++ Desktop/RexTech/MicroSoft clone/script.js
@@ -0,0 +1,7 @@
+'use strict';
+
+const section = document.querySelector('.section--1');
+const slides =document.querySelectorAll('.slide');
+
+// slides.forEach((s, i) => (s.style.transform = `translateX(${100 * i}%)`));
+section.style.transform = scale(0.2);
